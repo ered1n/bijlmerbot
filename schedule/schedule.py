88e6ded9843ab78eff190c795502bfe1d3bba912
Romano <rg_asciutto@hotmail.com>
2017-09-29 22:22:09 +0200

    class number is now variable

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -7,16 +7,28 @@ import hashlib
 
 dir = os.path.dirname(os.path.realpath(__file__)) + "/"
 
-week = str(date.today().isocalendar()[1])
-url = "https://rooster.talnet.nl/zuidoost/" + week + "/c/c00044.htm"
+nav = "https://rooster.talnet.nl/zuidoost/frames/navbar.htm"
 old_exists = Path(dir + "old.txt").is_file()
 filename = dir  + "old.txt"
 removeDates = "\n8.00"
+navTop = "var classes = "
+navBot = "\n var teachers = "
 
+def getWeek():
+    return date.today().isocalendar()[1]
+
+async def getClassNum():
+    async with aiohttp.ClientSession() as session:
+            async with session.get(nav) as response:
+                source = await response.text()
+                return eval(source.split(navTop)[1].split(navBot)[0].replace(";", "")).index("OITAOO5A") + 1
+          
 async def getSchedule():
+    week = getWeek()
+    classNum = await getClassNum()
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
+            async with session.get(f"https://rooster.talnet.nl/zuidoost/{week}/c/c000{classNum}.htm") as response:
                 source = await response.text()
                 return source.split(removeDates)[1]
     except IndexError:
