479503c992defc20ad076012fdca1a0857dcc314
Romano <rg_asciutto@hotmail.com>
2017-10-24 00:16:14 +0200

    implemented leaderboard pages and proper formatting

--- cogs/basic.py
+++ cogs/basic.py
@@ -1,5 +1,6 @@
 import discord
 import datetime
+import math
 
 from discord.ext import commands
 from methods import levels
@@ -57,21 +58,36 @@ class Basic:
             await ctx.send("Level up notification channel set :thumbsup:")
 
     @rank.command()
-    async def leaderboard(self, ctx):
-        data = await ctx.bot.db.fetch(f"SELECT user_id, xp FROM level WHERE server_id={ctx.guild.id} ORDER BY xp DESC LIMIT 50")
+    async def leaderboard(self, ctx, page: int=1):
+        data = await ctx.bot.db.fetch(f"SELECT user_id, xp FROM level WHERE server_id={ctx.guild.id} ORDER BY xp DESC")
+        amount_per_page = 10
+        page = page - 1
         output = "```"
-        for x in range(len(data)):
-            try:
-                output += "\n#" + str(x + 1) + " | " + str(await commands.MemberConverter().convert(ctx, str(data[x][0]))) + " | lvl: " + str(levels.get_level_from_xp(data[x][1])) + " | xp: " + str(data[x][1])
-            except commands.BadArgument:
-                pass
-        await ctx.send(output + "```")
+        if page < 0 or page * amount_per_page >= len(data):
+            await ctx.send("Error, page doesn't exist :disappointed_relieved:")
+        else:
+            data_page = []
+            counter = 1
+            for x in range(len(data)):
+                if x >= page * amount_per_page:
+                    if x >= page * amount_per_page + amount_per_page:
+                        break
+                    try:
+                        user = await commands.MemberConverter().convert(ctx, str(data[x][0]))
+                        data_page.append(["#" + str(page * amount_per_page + counter), user.display_name, str(levels.get_level_from_xp(data[x][1])), str(data[x][1])])
+                        counter += 1
+                    except commands.BadArgument:
+                        data_page.append(["#" + str(page * amount_per_page + counter), str(data[x][0]), str(levels.get_level_from_xp(data[x][1])), str(data[x][1])])
+                        counter += 1
+            for x in range(len(data_page)):
+                output += "\n" + f"{data_page[x][0]:<{max(len(x[0]) for x in data_page)}}" + " | " + f"{data_page[x][1]:<{max(len(x[1]) for x in data_page)}}" + " | lvl: " + f"{data_page[x][2]:<{max(len(x[2]) for x in data_page)}}" + " | xp: " + data_page[x][3]
+            await ctx.send(output + "\n\npage " + str(page + 1) + "/" + (str(math.ceil(len(data) / amount_per_page))) + "```")
 
     @commands.command()
     async def invite(self, ctx):
         embed = discord.Embed(color=0x00ff00)
         embed.add_field(name="Invite me to your server", value="[Invite](https://discordapp.com/oauth2/authorize?client_id=327170106318651392&scope=bot)", inline=False)
-        embed.add_field(name="Join my server", value="[Join](https://discord.gg/n7H8f5T)", inline=False)
+        embed.add_field(name="Join my server", value="[Join](https://discord.gg/CF7ArqD)", inline=False)
         await ctx.send(embed=embed)
 
 def setup(bot):
